ff45688b7195c21acb1001622c0cbd351031e7ea
Christine <yuaqing.hao@mail.mcgill.ca>
2023-11-19 20:15:25 -0500

    Finalized Login interface, put the boostrap CND, imported Vantajs for interactive and dynamic background, designed background color,, input button shapes. Added essential login information requireemnts and register option.

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
-import './Login.css'; 
+import React, { useState, useEffect, useRef } from 'react'; // Import useEffect and useRef here
+import './login.css';
+
 
 const Login = () => {
+  const [vantaEffect, setVantaEffect] = useState(null); // Initialize vantaEffect with null
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const vantaRef = useRef(null); // Create a ref for the Vanta effect
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(window.VANTA.BIRDS({
+        el: vantaRef.current,
+        THREE: window.THREE, // Assuming THREE.js is included in your index.html
+        // ... other Vanta.js options you might want to use
+      }));
+    }
+    // Cleanup function to destroy Vanta effect when the component unmounts
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]); // Dependency array ensures effect is only run once
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -15,28 +32,40 @@ const Login = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // Here we handle the login logic, possibly sending a request to our server
     console.log('Email:', email, 'Password:', password);
-    // Implement login functionality here
+    // Import verfiication code form Jiahao 
   };
 
   return (
-    <div className="login-container">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-          placeholder="Enter email"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder="Enter password"
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div class="row" ref={vantaRef}>
+      <section className="col-sm-10 col-md-8 col-lg-6 mx-auto login-container" >
+        
+        <p className="welcome-message">Welcome to Double Bound</p>
+          <br></br>
+          <form className="login-form" onSubmit={handleSubmit}>
+            <input
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              placeholder="Enter email"
+              required // Ensuring the input is filled
+            /><br></br>
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Enter password"
+              required // Ensuring the input is filled
+            /> <br></br>
+            <button type="submit">Login</button>
+          </form>
+
+          <br></br>
+          <p>Don't have an account? Register</p>
+        
+         
+      </section>
+    
     </div>
   );
 };
