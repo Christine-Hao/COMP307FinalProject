ca1fa64cb6e8009e0a2578fe595daa767cb5183d
Nicholas Foisy <nick319b@gmail.com>
2023-12-02 13:41:48 -0500

    updated css for navbar

--- client/src/components/Navbar/AccountInfoPopup.js
+++ client/src/components/Navbar/AccountInfoPopup.js
@@ -17,19 +17,18 @@ function AccountInfoPopup({ show, handleClose }) {
         <Modal.Body>
           <form>
               <div className="form-group">
-                <label htmlFor="display-name">Display name</label>
+                <label htmlFor="display-name" style={{ color: "black"}}>Display name</label>
                 <input type="text" className="form-control-lg" id="display-name" defaultValue="Eleftheri V." disabled/>
-                <div class="form-text">Display name to be updateable later maybe</div>
               </div>
               <div className="form-group">
-                <label htmlFor="name">Legal name</label>
+                <label htmlFor="name" style={{ color: "black"}}>Legal name</label>
                 <input type="text" className="form-control-lg" id="name" defaultValue="Eleftherios Venizelos" disabled/>
-                <div class="form-text">Your legal name cannot be updated.</div>
+                <div class="form-text account-text-info" style={{ fontSize: "15px" }}>Your legal name cannot be updated.</div>
               </div>
-              <div className="form-group">
+              <div className="form-group" style={{ color: "black"}}>
                 <label htmlFor="email">Email</label>
                 <input type="email" className="form-control-lg" id="email" defaultValue="elef.venizelos@gmail.com" disabled/>
-                <div class="form-text">Your email cannot be updated.</div>
+                <div class="form-text account-text-info" style={{ fontSize: "15px" }}>Your email cannot be updated.</div>
               </div>
           </form>
         </Modal.Body>
